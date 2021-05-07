e2b8ff60b9aaaed7dc8a1de0c7bbcacb5b0a4a5a
rajesh6161 <rajeshranjan2712000@gmail.com>
2021-05-07 08:53:27 +0530

    more changes

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -101,6 +101,14 @@ class MealDetailScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.delete,
+        ),
+        onPressed: () {
+          Navigator.of(context).pop(mealId);
+        },
+      ),
     );
   }
 }
