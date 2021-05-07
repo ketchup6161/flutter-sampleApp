d1b42fcf165ce9d08eb24abf20c5854a9b849ca0
rajesh6161 <rajeshranjan2712000@gmail.com>
2021-05-07 11:11:42 +0530

    manifest.xml

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -33,7 +33,7 @@ class MainDrawer extends StatelessWidget {
             alignment: Alignment.centerLeft,
             color: Theme.of(context).accentColor,
             child: Text(
-              'Cooking Up!',
+              'Food App',
               style: TextStyle(
                 fontWeight: FontWeight.w900,
                 fontSize: 30,
